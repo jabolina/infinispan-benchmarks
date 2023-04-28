7ffb01dbc271535161fea40b6d9e897c536146d3
jabolina <jbolina@redhat.com>
2023-04-28 11:12:58 -0300

    Verifying with refactor changes

--- resp-decoder/src/main/java/org/infinispan/server/resp/OurRespHandler.java
+++ resp-decoder/src/main/java/org/infinispan/server/resp/OurRespHandler.java
@@ -1,32 +1,32 @@
 package org.infinispan.server.resp;
 
+import static org.infinispan.server.resp.ByteBufferUtils.bytesToResult;
+import static org.infinispan.server.resp.ByteBufferUtils.stringToByteBuf;
+
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.IntFunction;
 
 import org.infinispan.commons.util.concurrent.CompletableFutures;
-import org.infinispan.util.function.TriConsumer;
+import org.infinispan.server.resp.commands.string.GET;
+import org.infinispan.server.resp.commands.string.SET;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.AttributeKey;
 
 public class OurRespHandler extends RespRequestHandler {
    private static final CompletableFuture<byte[]> GET_FUTURE = CompletableFuture.completedFuture(new byte[] { 0x1, 0x12});
    public static byte[] OK = "+OK\r\n".getBytes(StandardCharsets.US_ASCII);
 
    @Override
-   public CompletionStage<RespRequestHandler> actualHandleRequest(ChannelHandlerContext ctx, String type, List<byte[]> arguments) {
-      switch (type) {
-         case "GET":
-            return stageToReturn(GET_FUTURE, ctx, GET_TRICONSUMER);
-         case "SET":
-            return stageToReturn(CompletableFutures.completedNull(), ctx, OK_BICONSUMER);
-      }
+   protected CompletionStage<RespRequestHandler> actualHandleRequest(ChannelHandlerContext ctx, RespCommand type, List<byte[]> arguments) {
+      if (type instanceof GET)
+         return stageToReturn(GET_FUTURE, ctx, GET_TRICONSUMER);
+
+      if (type instanceof SET)
+         return stageToReturn(CompletableFutures.completedNull(), ctx, OK_BICONSUMER);
+
       return super.handleRequest(ctx, type, arguments);
    }
 
